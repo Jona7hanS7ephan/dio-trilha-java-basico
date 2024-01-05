ac93e4fd30881a88617f0072b648efaf2c028bab
jonathan.stephan <julisses2@gmail.com>
2024-01-05 12:35:52 -0300

    teste

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
+import java.sql.Date;
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+    	double soma = 11 +15;
+    	int sub = 113 - 10;
+    	System.out.println(sub - soma);
     }
 }
